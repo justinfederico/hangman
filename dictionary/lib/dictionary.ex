0869f28dfcc3cce8bcc65815b84bb39660dd6d4d
justinfederico <jfederico0241@sdsu.edu>
2022-05-25 16:23:13 -0700

    Enter

--- dictionary/lib/dictionary.ex
+++ dictionary/lib/dictionary.ex
@@ -1,5 +1,11 @@
 defmodule Dictionary do
-  def hello do
-    IO.puts "Hello world"
+
+  @word_list "assets/words.txt" # path to word list
+    |> File.read!() # use elixir pipeline to pass path in
+    |> String.split(~r/\n/, trim: true) # augment file contents
+
+  def random_word do
+    @word_list
+    Enum.random()
   end
 end
